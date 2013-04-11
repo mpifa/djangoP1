4ea7816af6bb43cd502479d91ab3340d98f8dbc9
marc <marc@marc.(none)>
2013-04-12 01:52:21 +0200

    Still fixing bugs

--- main/views.py
+++ main/views.py
@@ -8,6 +8,7 @@ from django.template import Context
 from django.template.loader import get_template
 from main.models import *
 from datetime import datetime
+from django.http import Http404
 from main import *
 
 
@@ -26,91 +27,148 @@ def mainpage(request):
 
 def pc(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.filter(platform='PC')   
-    variables =Context({
-        'Title':"List of PC Games",
-        'TYPE':'pc',
-        'result': collection,
-        'user': request.user,
-        
-    })
+    tmp='PC'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+            
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 
 def xbox360(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.all().filter(platform='Xbox 360')
-    variables =Context({
-        'Title':"List of Xbox 360 Games",
-        'TYPE':'xbox360',
-        'result': collection,
-        'user': request.user,
-    })
+    tmp='Xbox 360'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 
 def ps3(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.all().filter(platform='PlayStation 3')
-    variables =Context({
-        'Title':"List of PlayStation 3 Games",
-        'TYPE':'ps3',
-        'result': collection,
-        'user': request.user,
-    })
+    tmp='PlayStation 3'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+            
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 def wii(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.all().filter(platform='Wii')
-    variables =Context({
-        'Title':"List of Wii Games",
-        'TYPE':'wii',
-        'result': collection,
-        'user': request.user,
-    })
+    tmp='Wii'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+            
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 def vita(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.all().filter(platform='Vita')
-    variables =Context({
-        'Title':"List of Play Station Portable Vita Games",
-        'TYPE':'vita',
-        'result': collection,
-        'user': request.user,
-    })
+    tmp='PSP'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+            
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 def n3ds(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.all().filter(platform='N3ds')
-    variables =Context({
-        'Title':"List of Nintendo 3DS Games",
-        'TYPE':'n3ds',
-        'result': collection,
-        'user': request.user,
-    })
+    tmp='Nintendo DS'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+            
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 def mobile(request):
     template = get_template('info.html')
-    collection = SupportedBy.objects.all().filter(platform='Mobile')
-    variables =Context({
-        'Title':"List of Mobile Games",
-        'TYPE':'mobile',
-        'result': collection,
-        'user': request.user,
-    })
+    tmp='Mobile'
+    try:
+        coll = Made.objects.get(platform=tmp)
+        collection = SupportedBy.objects.all().filter(platform=tmp)
+            
+        variables =Context({
+            'Title':'List of '+tmp+' Games',
+            'TYPE':tmp,
+            'Date': coll.platform.releaseDate,
+            'Made':coll.company.name,
+            'set':coll.company.setUp,
+            'result': collection,
+            'user': request.user,
+        })
+    except:
+        raise Http404
     return render_to_response('info.html',variables)
 
 
-def gameDetails(request,ref):
-    #template = get_template('details.html')
-    #gameInfo = SupportedBy.objects.get(game=ref)
-    #Types = BelongsTo.objects.get(game=ref)
-    #Publisher = Game.objects.get(name=ref)
-    
+
+def gameDetails(request,ref):   
     template = get_template('details.html')
     gameInfo = SupportedBy.objects.all().filter(game=ref)
     Types = BelongsTo.objects.filter(game=ref)
@@ -132,6 +190,58 @@ def gameDetails(request,ref):
         'user': request.user,
     })
     return render_to_response('details.html',variables)
+
+def gameByType(request,ref):
+    Types = BelongsTo.objects.filter(Type=ref)
+    
+    g=[]
+    games=({})
+    for t in Types:
+        g.append(str(t.game.name))
+        for ts in g:
+            games=SupportedBy.objects.filter(game=ts)
+          
+    #plat = SupportedBy.objects.filter(name='')
+    variables=Context({
+        'Title': 'All '+ref+' games',
+        'TYPE': games,
+        'result': g,
+        'user': request.user,
+
+        })
+       
+    return render_to_response('details2.html',variables)
+
+def gameByCompany(request,ref):
+    Company = Game.objects.filter(publisher=ref)
+    g=[]
+    gl=({})
+    llista=[]
+    typelist=[]
+    llist=[]
+    for game in Company:
+        g.append(str(game.name))
+    for t in g:
+        gamelist=SupportedBy.objects.filter(game=t)
+        typelist=[]
+        for x in gamelist:
+            typelist.append(x.platform.name)
+        gl.update({x.game.name:typelist})
+        llist=[]
+        for i in gl.get(t):
+            llist.append(i)
+        llista.append(llist)
+    print llista
+    variables=Context({
+        'Title': 'All '+ref+' games',
+        'TYPE': llista,
+        'result': g,
+        'user': request.user,
+
+        })
+    
+    return render_to_response('details3.html',variables)
+
 
 def userpage(request,username):
     try:
